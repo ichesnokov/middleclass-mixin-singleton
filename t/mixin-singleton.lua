d951b129ef91cef9cd71324fbea213a5034d9975
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2018-03-31 16:13:00 +0300

    Tests

---
+++ t/mixin-singleton.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env lua
+package.path = package.path .. ';./?.lua'
+require 'Test.More'
+
+local class = require('middleclass')
+local CoolClass = class('CoolClass'):include(require('middleclass.mixin.singleton'))
+
+function CoolClass:initialize(version)
+  self.name    = '-=[cool class]=-'
+  self.version = version
+end
+
+ok(CoolClass, 'Construct CoolClass')
+
+local obj1 = CoolClass:instance('1.0.2')
+ok(obj1:isInstanceOf(CoolClass), 'First instance is an instance of SomeClass')
+is(obj1.name, '-=[cool class]=-', '...and initialize() was called')
+is(obj1.version, '1.0.2', '...and proper argument was passed to it')
+
+local obj2 = CoolClass:instance()
+ok(obj2:isInstanceOf(CoolClass), 'Second object is also a CoolClass instance')
+ok(obj1 == obj2, 'Both instances are the same object')
+
+error_like(
+  CoolClass.new,
+  'Use CoolClass:instance.*instead of :new',
+  'We get a proper error if we try to construct an instance with :new()'
+)
+
+done_testing()
